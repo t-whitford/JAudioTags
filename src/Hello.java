1286b1996e0619d10761e8f1d5941d2140187b12
Tom Whitford <whity4420@gmail.com>
2015-12-09 17:09:17 +0000

    HelloWorld commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Tom Whitford on 09/12/2015.
+ */
+public class Hello {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+
+}
