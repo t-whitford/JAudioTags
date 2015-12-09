117045edcd929008d1513a9e27feeefefa3e2eb3
Tom Whitford <whity4420@gmail.com>
2015-12-09 17:31:17 +0000

    Testing IntelliJ with git

--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,6 @@ public class Hello {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+        System.out.println("Test IntelliJ git setup");
     }
-
 }
